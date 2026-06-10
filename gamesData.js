e635b87d4492aa7035d069414bb212deea25e277
denshust <50724048+denshust@users.noreply.github.com>
2026-06-10 18:39:03 +0200

    Add files via upload

---
+++ gamesData.js
@@ -0,0 +1,57 @@
+// gamesData.js
+const gamesList = [
+    {
+        id: "doomsday",        
+        title: "DOOMSDAY",
+        author: "tolokgame",
+        description: "Рви і трощи ворогів на своєму шляху! У тебе є повноцінний арсенал зброї та система прогресії. Вбий боса на 5-му рівні, щоб отримати мініган, а на 10-му рівні -  головну здатність гри: уповільнення часу.",
+        image: "img/AKaEzt.png",
+        itchUrl: "https://tolokgame.itch.io/doomsday",
+        place: 1
+    },
+    {
+        id: "breach_game",
+        title: "Breach game",
+        author: "User",
+        description: "Система безопасности корпорации NEXUS вышла из-под контроля. Твоя задача — внедриться в распределенные узлы сети, используя брутфорс, криптоанализ и обход физических цепей брандмауэра.",
+        image: "img/AmPcBH.png",
+        itchUrl: "https://user110611.itch.io/breach-game",
+        place: 2
+    },
+    {
+        id: "forward_dream",
+        title: "Forward to your dream!",
+        author: "MRMIL",
+        description: "Динамічна космічна аркада. Керуйте рибкою, долайте космічні перешкоди та рухайтеся назустріч своїй мрії!",
+        image: "img/1osr_V.png",
+        itchUrl: "https://mrmil.itch.io/forward-to-your-dream",
+        place: 3
+    },
+    {
+        id: "devils_miracle",
+        title: "Devil's Miracle",
+        author: "SoHatter",
+        description: "Це простенька гра у жанрі Boss Rush. Головною фішкою гри є те що головний персонаж підписав угоду з дияволом і отримав сили чудес. Ці сили чудес мають 7 зарядів, прямо як і 7 чудес світу.",
+        image: "img/zqPZbi.png",
+        itchUrl: "https://sohatter.itch.io/devils-miracle",
+        place: 4
+    },
+    {
+        id: "wonder",
+        title: "WONDER",
+        author: "ZyreX",
+        description: "атмосферна пригодницька гра, у якій ти повертаєш світло в помираючий світ. Досліджуй темряву, збирай Wonder Energy, виживай серед тіньових духів та відновлюй життя протягом 10 дедалі складніших хвиль.",
+        image: "img/9XS9Lb.png",
+        itchUrl: "https://xxx-pavlo-xxx.itch.io/wonder",
+        place: 5
+    },
+    {
+        id: "zhaba",
+        title: "жаба",
+        author: "Oleksiy228",
+        description: "Чи зможеш ти дістися жабкою до KFC? Відправляйся в епічну подорож, долаючи перешкоди на шляху до смачної нагороди!",
+        image: "img/_Vf_iQ.png",
+        itchUrl: "https://oleksiy228.itch.io/675242228",
+        place: 6
+    }
+];
